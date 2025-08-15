da5648eeb0815da52c51e023d3756c5c69ea8e81
trobukan <trobukan@gmail.com>
2025-08-15 19:15:33 -0300

    feat: state and events handlers for fetching dog data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,72 @@
-import "./App.css"
+import { useEffect, useState } from "react";
+import { GetBreedList, GetImageUrlsByBreed, GetRandomImageUrl } from "../wailsjs/go/main/App";
+import "./App.css";
 
 function App() {
-     return (
-    <div>
-      <h3>Dogs API</h3>
-      <div>
-        <button>Fetch a dog randomly</button>
-        <select></select>
-        <button>Fetch by this breed</button>
-      </div>
-      <br/>
-      <div>
-      </div>
-    </div>
-  );
+    const [breeds, setBreeds] = useState<string[]>([]);
+    const [photos, setPhotos] = useState<string[]>([]);
+
+    const [randomImageUrl, setRandomImageUrl] = useState("");
+    const [selectedBreed, setSelectedBreed] = useState("");
+    const [showRandomPhoto, setShowRandomPhoto] = useState(false);
+    const [showBreedPhotos, setShowBreedPhotos] = useState(false);
+
+    useEffect(() => {
+        async function getInitialBreedList() {
+            const result = await GetBreedList();
+            setBreeds(result);
+        };
+        getInitialBreedList();
+    }, [])
+
+    async function getRandomImageUrl() {
+        setShowRandomPhoto(false);
+        setShowBreedPhotos(false);
+
+        const result = await GetRandomImageUrl();
+        setRandomImageUrl(result);
+        setShowRandomPhoto(true);
+    }
+
+    async function getImageUrlsByBreed() {
+        setShowRandomPhoto(false);
+        setShowBreedPhotos(false);
+
+        const result = await GetImageUrlsByBreed(selectedBreed)
+        setPhotos(result)
+        setShowBreedPhotos(true);
+    }
+    return (
+        <>
+            <h3>Dogs API</h3>
+            <div>
+                <button className="btn" onClick={getRandomImageUrl}>
+                    Fetch a dog randomly
+                </button>
+                <select value={selectedBreed} onChange={(e) => setSelectedBreed(e.target.value)}>
+                    {breeds.map((breed) => (
+                        <option key={breed} value={breed}>
+                            {breed}
+                        </option>
+                    ))}
+                </select>
+                <button className="btn" onClick={getImageUrlsByBreed}>
+                    Fetch by this breed
+                </button>
+            </div>
+            <br />
+            {showRandomPhoto && (
+                <img id="random-photo" src={randomImageUrl} alt="No dog found" />
+            )}
+            {showBreedPhotos && (
+                <>
+                    {photos.map((photo) => (
+                        <img id="breed-photos" key={photo} src={photo} alt="No dog found" />
+                    ))}
+                </>
+            )}
+        </>
+    );
 }
 
-export default App
+export default App;
